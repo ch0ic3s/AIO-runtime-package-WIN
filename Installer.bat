5065929f0e50bc73e5d65cf22339779a52cf5f38
ch0ic3s <100861853+ch0ic3s@users.noreply.github.com>
2022-03-04 12:36:52 +0000

    Update Installer.bat
    compacts code

--- Installer.bat
+++ Installer.bat
@@ -1,23 +1,18 @@
 @echo off
 setlocal EnableDelayedExpansion
-
 ::net file to test privileges, 1>NUL redirects output, 2>NUL redirects errors
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' ( goto START ) else ( goto getPrivileges ) 
-
 :getPrivileges
 if '%1'=='ELEV' ( goto START )
-
 set "batchPath=%~f0"
 set "batchArgs=ELEV"
-
 ::Add quotes to the batch path, if needed
 set "script=%0"
 set script=%script:"=%
 IF '%0'=='!script!' ( GOTO PathQuotesDone )
     set "batchPath=""%batchPath%"""
 :PathQuotesDone
-
 ::Add quotes to the arguments, if needed.
 :ArgLoop
 IF '%1'=='' ( GOTO EndArgLoop ) else ( GOTO AddArg )
@@ -33,7 +28,6 @@ IF '%1'=='' ( GOTO EndArgLoop ) else ( GOTO AddArg )
     shift
     GOTO ArgLoop
 :EndArgLoop
-
 ::Create and run the vb script to elevate the batch file
 ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
 ECHO UAC.ShellExecute "cmd", "/c ""!batchPath! !batchArgs!""", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
@@ -98,10 +92,8 @@ start /wait XNA30.msi /passive /norestart
 start /wait XNA40.msi /passive /norestart
 start /wait oalinst.exe -s
 goto END
-
 :VCR
 set IS_X64=0 && if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set IS_X64=1) else (if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (set IS_X64=1))
-
 if "%IS_X64%" == "1" goto 64b
 start /wait vcredist2005_x86.exe /q
 start /wait vcredist2008_x86.exe /qb
