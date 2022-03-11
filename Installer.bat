d66cc68432c53b45d91b6a479a791f18a236d60e
ch0ic3s <100861853+ch0ic3s@users.noreply.github.com>
2022-03-11 13:56:09 +0000

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -61,17 +61,17 @@ CLS
 CLS
 color 17
 echo ########################################################
-ECHO #ch0ic3s' AIO runtime V1.5                             #
+ECHO # ch0ic3s' AIO runtime V1.5                            #
 echo ########################################################
-ECHO #PRESS 1,2,3,4,5,6 or 7                                #
+ECHO # PRESS 1,2,3,4,5,6 or 7                               #
 ECHO ########################################################
-ECHO #1 - Install VCRedist,XNAframework,OpenAL and DirectX  #
-ECHO #2 - Install XNAframework                              #
-ECHO #3 - Install VCRedist                                  #
-ECHO #4 - Install OpenAL                                    #
-echo #5 - Install DirectX                                   #
-echo #6 - View changelog                                    #
-ech0 #7 - Exit                                              #
+ECHO # 1 - Install VCRedist,XNAframework,OpenAL and DirectX #
+ECHO # 2 - Install XNAframework                             #
+ECHO # 3 - Install VCRedist                                 #
+ECHO # 4 - Install OpenAL                                   #
+echo # 5 - Install DirectX                                  #
+echo # 6 - View changelog                                   #
+ech0 # 7 - Exit                                             #
 echo ########################################################
 ECHO.
 SET /P M=Type 1, 2, 3, 4 or 5 then press ENTER:
